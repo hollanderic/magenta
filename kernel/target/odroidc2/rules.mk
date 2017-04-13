ee8bfc3c17e2aaa32d4f2ab60d6f869551e85127
Eric Holland <hollande@google.com>
2017-04-13 16:35:09 -0700

    [first cut at odroid generic arm]
    Change-Id: I99327c72c2db091824552cad812ddbea21c01aa2

---
+++ kernel/target/odroidc2/rules.mk
@@ -0,0 +1,41 @@
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+PLATFORM := generic-arm
+
+MEMBASE := 0x00000000
+MEMSIZE ?= 0x80000000   # 2GB
+KERNEL_LOAD_OFFSET := 0x11000000 # whatever grub wanted
+
+PERIPH_BASE_PHYS := 0xc0000000U
+PERIPH_SIZE := 0x10200000UL
+PERIPH_BASE_VIRT := 0xffffffffc0000000ULL
+MEMORY_APERTURE_SIZE  := 0xc0000000UL
+
+BOOTLOADER_RESERVE_START := 0
+BOOTLOADER_RESERVE_SIZE := 0x100000
+
+KERNEL_DEFINES += \
+    MEMBASE=$(MEMBASE) \
+    MEMSIZE=$(MEMSIZE) \
+	PERIPH_BASE_PHYS=$(PERIPH_BASE_PHYS) \
+	PERIPH_BASE_VIRT=$(PERIPH_BASE_VIRT) \
+	PERIPH_SIZE=$(PERIPH_SIZE) \
+	MEMORY_APERTURE_SIZE=$(MEMORY_APERTURE_SIZE) \
+	BOOTLOADER_RESERVE_START=$(BOOTLOADER_RESERVE_START) \
+	BOOTLOADER_RESERVE_SIZE=$(BOOTLOADER_RESERVE_SIZE) \
+    PLATFORM_SUPPORTS_PANIC_SHELL=1 \
+
+# build MDI
+MDI_SRCS := \
+    $(LOCAL_DIR)/odroidc2.mdi \
+
+MDI_DEPS := \
+    kernel/include/mdi/kernel-defs.mdi \
+
+include make/mdi.mk
