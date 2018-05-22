aec694fffcca861313da0be2ec9310c5aade0c8a
Eric Holland <hollande@google.com>
2018-05-22 16:36:20 -0700

    start of amltdm driver
    Change-Id: Iffee4d37e247ea093ceb24d38abf7b4ac165c46e

---
+++ system/dev/soc/amlogic/include/soc/aml-common/aml-tdm.h
@@ -0,0 +1,12 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include<ddk/metadata.h>
+
+//dTDM
+#define AML_TDM_METADATA (0x4d445400 | DEVICE_METADATA_DRIVER_DATA)
+
+
