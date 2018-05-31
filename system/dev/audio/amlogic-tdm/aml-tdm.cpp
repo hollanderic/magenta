1b7a7945f4bdc96624c2dffc516536969d5a3efb
Eric Holland <hollande@google.com>
2018-05-31 12:27:22 -0400

    [aml][audio] aml tdm driver

---
+++ system/dev/audio/amlogic-tdm/aml-tdm.cpp
@@ -0,0 +1,42 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <audio-proto-utils/format-utils.h>
+#include <ddk/debug.h>
+#include <ddk/device.h>
+#include <fbl/limits.h>
+#include <string.h>
+#include <zircon/device/audio.h>
+#include <lib/zx/vmar.h>
+#include <ddk/protocol/platform-device.h>
+
+#include "aml-tdm.h"
+
+fbl::unique_ptr<AmlTdmDevice> AmlTdmDevice::Create(platform_device_protocol_t* pdev,
+                                  uint32_t mmio_idx) {
+
+    auto tdm_dev = fbl::unique_ptr<AmlTdmDevice>(new AmlTdmDevice());
+
+    tdm_dev->uregs_ = ddk::MmioBlock<uint32_t>::Create(pdev, mmio_idx);
+    if (!tdm_dev->uregs_) {
+        zxlogf(ERROR,"AmlTdm: Failed to map mmio\n");
+        return nullptr;
+    }
+
+    tdm_dev->InitRegs();
+
+    return tdm_dev;
+}
+
+
+void AmlTdmDevice::InitRegs() {
+
+    uregs_->SetBits(0x00002000, AML_TDM_CLK_GATE_EN);
+
+
+}
+
+AmlTdmDevice::~AmlTdmDevice() {
+
+}
